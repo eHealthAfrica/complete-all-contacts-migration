893af2bb12e164c76f9f34a75fdbd6c0336a9593
Tibor Claassen <tibor@gmah.net>
2015-06-10 12:37:39 +0200

    Migrate all docs

--- bin/main.js
+++ bin/main.js
@@ -2,4 +2,11 @@
 
 var cli = require('../lib/cli')
 
-cli().run(process.argv.slice(2))
+cli()
+  .run(process.argv.slice(2))
+  .then(function (response) {
+    console.log("Done.")
+  })
+  .catch(function (error) {
+    console.error("Error:", error.message)
+  })
--- test/migrate.js
+++ test/migrate.js
@@ -2,7 +2,25 @@
 
 var test = require('tape')
 
-test('does something', function (t) {
-  t.fail('not implemented')
+var migrate = require('../lib/migrate')
+
+test('skips docs that are no contact', function (t) {
+  var doc = {}
+  var result = migrate(doc)
+  t.equal(result, null)
+  t.end()
+})
+
+test('skips docs with contact that is already complete', function (t) {
+  var doc = { contact: { status: 'complete' } }
+  var result = migrate(doc)
+  t.equal(result, null)
+  t.end()
+})
+
+test('sets status of contact to be complete', function (t) {
+  var doc = {_id: '1234', contact: {status: 'active'}}
+  var result = migrate(doc)
+  t.deepEqual(result, [{_id: '1234', contact: {status: 'complete'}}])
   t.end()
 })
